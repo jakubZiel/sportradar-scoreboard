56eda04d0d63a93bfc7241ed8f3af57feb3b0595
zielinsj <jakub.zielinski@webfleet.com>
2025-01-19 23:22:44 +0100

    init repo

---
+++ src/main/java/com/sportradar/scoreboard/ScoreboardApplication.java
@@ -0,0 +1,11 @@
+package com.sportradar.scoreboard;
+
+public class ScoreboardApplication
+{
+
+    public static void main(String[] args)
+    {
+        System.err.println("hello world!");
+    }
+
+}
