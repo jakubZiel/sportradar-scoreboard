bfb4a5dc9de029f6dab5d064f4a297aa7a414d5f
zielinsj <jakub.zielinski@webfleet.com>
2025-01-23 02:52:54 +0100

    simplify Game state object

--- src/main/java/com/sportradar/scoreboard/domain/game/Game.java
+++ src/main/java/com/sportradar/scoreboard/domain/game/Game.java
@@ -1,25 +1,16 @@
 package com.sportradar.scoreboard.domain.game;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
 import java.util.Map;
 
-import com.sportradar.scoreboard.domain.team.CardSet;
-import com.sportradar.scoreboard.domain.team.Squad;
+import com.sportradar.scoreboard.domain.team.Team;
 
 import lombok.Builder;
 import lombok.NonNull;
 
 
 @Builder(setterPrefix = "with", toBuilder = true)
-public record Game(GameKey key,
-                   Map<Long, @NonNull Integer> teamIdToScore,
-                   Map<Long, Squad> teamIdToSquad,
-                   Map<Long, CardSet> teamIdToCardSet,
-                   Duration currentMatchTime,
-                   @NonNull Instant startTime,
-                   List<Duration> overtimes,
-                   Instant closeTime)
+public record Game(@NonNull GameKey key,
+                   @NonNull Map<Team, @NonNull Integer> teamIdToScore,
+                   int totalScore)
 {
 }
